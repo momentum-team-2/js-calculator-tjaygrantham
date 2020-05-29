209265ead00868c65e27beaa7f9d21770bac5778
Tyler Grantham <tjaygrantham@gmail.com>
2020-05-28 22:09:31 -0400

    use math & add random color palette

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 let display = document.getElementById("display").getElementsByTagName("div")[0];
 
+/*let changeBtn = document.createElement('button')
+changeBtn.innerText = 'Change Color Palette';
+changeBtn.id = 'change-palette';
+document.getElementsByClassName('flex-container')[0].appendChild(changeBtn);*/
+
 for(btn of document.getElementsByTagName("button")){
     btn.onclick = event => {
+        if(display.innerText === 'Error')
+            display.innerText = '';
         btn = event.target
-        if(btn.id !== 'func'){
+        if(btn.id === 'change-palette'){
+            updatePalette();
+        }
+        else if(btn.id !== 'func'){
             let btntext = btn.innerText;
             if(btntext === 'x')
                 btntext = '*';
@@ -18,8 +28,59 @@ for(btn of document.getElementsByTagName("button")){
     }
 }
 
+let primaryColor;
+let secondaryColor;
+
+function updatePalette(){
+    let url = 'http://colormind.io/api/';
+    let data = {
+        model : 'default',
+        input : ['N', 'N']
+    };
+    let http = new XMLHttpRequest();
+    http.onreadystatechange = () => {
+        if(http.readyState == 4 && http.status == 200){
+            var palette = JSON.parse(http.responseText).result.map(rgb => {
+                return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+            });
+            primaryColor = palette[0];
+            secondaryColor = palette[1];
+        }
+        document.getElementById('display').style.backgroundColor = primaryColor;
+        for(btn of document.getElementsByTagName("button")){
+            if(btn.id === 'func' || btn.id === 'op')
+                btn.style.backgroundColor = secondaryColor;
+            else
+                btn.style.backgroundColor = primaryColor;
+        }
+    }
+    http.open("POST", url, true);
+    http.send(JSON.stringify(data));
+}
+updatePalette();
+for(btn of document.getElementsByTagName("button")){
+    btn.addEventListener('mouseover', event => switchColor(event.target) );
+    btn.addEventListener('mouseout', event => switchColor(event.target) );
+}
+
+function switchColor(elem){
+    if(elem.style.backgroundColor == primaryColor)
+        elem.style.backgroundColor = secondaryColor;
+    else
+        elem.style.backgroundColor = primaryColor;
+}
+
 function calculate(input){
-    updateDisplay(String(eval(input)))
+    let result;
+    try{
+        result = math.evaluate(input);
+    }
+    catch{
+        result = 'Error';
+    }
+    finally{
+        updateDisplay(String(result));
+    }
 }
 
 function updateDisplay(text){
